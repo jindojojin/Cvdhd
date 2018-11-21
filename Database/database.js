523c1d4ef722e6546441dd48b1ccfab65626b5e5
linhtq <tranquanglinh.pt@gmail.com>
2018-11-21 18:20:19 +0700

    add breeder, get account

--- Database/database.js
+++ Database/database.js
@@ -2,6 +2,39 @@ var mongoClient = require('mongodb').MongoClient;
 const url='mongodb://server:NSzE8QWym39iQTL@ds041347.mlab.com:41347/cvdhd';
 var ObjectId = require('mongodb').ObjectID;
 var dbmodel= {
+    addBreeder: async function(username,password,BreederInfo){
+        let client = await mongoClient.connect(url,{useNewUrlParser : true});
+        let db = client.db('cvdhd');
+        try {    
+            BreederInfo.defaultCowGroup=[];
+            BreederInfo.otherCowGroup=[];       
+            let breederInserted = await db.collection('Breeders').insertOne(BreederInfo);
+            let accInserted = await db.collection('Accounts').insertOne({
+                "username":username,
+                "password":password,
+                "type":"breeder",
+                "breederID":breederInserted.insertedId+""
+            })
+        } catch (error) {
+            return Promise.reject(error);
+        } finally {
+            client.close();
+        }
+    },
+    getAccount: async function(username){
+        let client = await mongoClient.connect(url,{useNewUrlParser : true});
+        let db = client.db('cvdhd');
+        try {            
+            let query = {username:username};
+            let acc = await db.collection('Accounts').findOne(query);
+            console.log(acc);
+            return Promise.resolve(acc);
+        } catch (error) {
+            return Promise.reject(error);
+        } finally {
+            client.close();
+        }
+    },
 
     addCow: async function(BreederID,CowInfo){
         if(! ObjectId.isValid(BreederID)) return Promise.reject("invalid BreederID");
@@ -69,7 +102,10 @@ var dbmodel= {
  
 }
 // dbmodel.addGroup("5bf52190fb6fc0561ffdc0e5",{"name":"Nhóm bò nhập về ngày 20/10/2020","cow":[]});
-dbmodel.addCowToGroup("5bf52354e1b6fd16f44c7da1","5bf52f64f1abf42620b2c641","5bf52ab93a38bb393062f63e");
+// dbmodel.addCowToGroup("5bf52354e1b6fd16f44c7da1","5bf52f64f1abf42620b2c641","5bf52ab93a38bb393062f63e");
+// dbmodel.getAccount("admin1");
+dbmodel.addBreeder("breeder2","breeder2",{
+"name" : "Trần Quang Linh"})
 // dbmodel.getBannedWebSites('5bf4abcae7179a56e213cd2d');
 // dbmodel.addUser("5bf3ec04e7179a56e21350f3","May02");
 
