1e8813de3f448cd5f7f1422edd110f38d317c3af
linhtq <tranquanglinh.pt@gmail.com>
2018-12-05 19:27:12 +0700

    add template

--- index.js
+++ index.js
@@ -3,43 +3,43 @@ function changePage(a){
     if(a == 'addCow'){
         var router = document.getElementById("router");
         // router.removeChild();
-        router.innerHTML='<iframe width="100%" height="100%" frameborder="0" src="CowComponent/AddCow/addcow.html"></iframe>';
+        router.innerHTML='<iframe style="overflow:hidden;display:block; position: absolute; height: 100%; width: 100%" frameborder="0" src="CowComponent/AddCow/addcow.html"></iframe>';
         return;
     }
     if(a == 'home'){
         var router = document.getElementById("router");
         // router.removeChild();
-        router.innerHTML='<iframe width="100%" height="100%" frameborder="0" src="index.html"></iframe>';
+        router.innerHTML='<iframe style="overflow:hidden;display:block; position: absolute; height: 100%; width: 100%" frameborder="0" src="index.html"></iframe>';
         return;
     }
     if(a == 'foods'){
         var router = document.getElementById("router");
         // router.removeChild();
-        router.innerHTML='<iframe width="100%" height="100%" frameborder="0" src="CowComponent/Food/food.html"></iframe>';
+        router.innerHTML='<iframe style="overflow:hidden;display:block; position: absolute; height: 100%; width: 100%" frameborder="0" src="CowComponent/Food/food.html"></iframe>';
         return;
     }
     if(a == 'cowHealth'){
         var router = document.getElementById("router");
         // router.removeChild();
-        router.innerHTML='<iframe width="100%" height="100%" frameborder="0" src="CowComponent/CowHealth/cowhealth.html"></iframe>';
+        router.innerHTML='<iframe style="overflow:hidden;display:block; position: absolute; height: 100%; width: 100%" frameborder="0" src="CowComponent/CowHealth/cowhealth.html"></iframe>';
         return;
     }
     if(a == 'image'){
         var router = document.getElementById("router");
         // router.removeChild();
-        router.innerHTML='<iframe width="100%" height="100%" frameborder="0" src="CowComponent/Image/image.html"></iframe>';
+        router.innerHTML='<iframe style="overflow:hidden;display:block; position: absolute; height: 100%; width: 100%" frameborder="0" src="CowComponent/Image/image.html"></iframe>';
         return;
     }
     if(a == 'vaccine'){
         var router = document.getElementById("router");
         // router.removeChild();
-        router.innerHTML='<iframe width="100%" height="100%" frameborder="0" src="CowComponent/Vaccine/vaccine.html"></iframe>';
+        router.innerHTML='<iframe style="overflow:hidden;display:block; position: absolute; height: 100%; width: 100%" frameborder="0" src="CowComponent/Vaccine/vaccine.html"></iframe>';
         return;
     }
     if(a == 'sellCow'){
         var router = document.getElementById("router");
         // router.removeChild();
-        router.innerHTML='<iframe width="100%" height="100%" frameborder="0" src="CowComponent/SellCow/sellcow.html"></iframe>';
+        router.innerHTML='<iframe style="overflow:hidden;display:block; position: absolute; height: 100%; width: 100%" frameborder="0" src="CowComponent/SellCow/sellcow.html"></iframe>';
         return;
     }
 }
